a1f920bf7fe9333ec9bb9ab0a4b16ca7e88bb76d
Jason <jasona99@outlook.com>
2019-03-02 16:22:01 -0500

    Early SDK additions and functionality

--- app/src/main/java/com/auxilium/hack/MainActivity.java
+++ app/src/main/java/com/auxilium/hack/MainActivity.java
@@ -1,7 +1,16 @@
 package com.auxilium.hack;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+import com.google.firebase.*;
+import com.auxilium.hack.CameraPermissionHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +20,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+    private boolean mUserRequestedInstall = true;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Check camera permission.
+        // ARCore requires camera permissions to operate. If we did not yet obtain runtime
+        // permission on Android M and above, now is a good time to ask the user for it.
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this);
+            return;
+        }
+
+        if (!LocationPermissionHelper.hasLocationPermission(this)) {
+            LocationPermissionHelper.requestLocationPermission(this);
+            return;
+        }
+        // Set to true ensures requestInstall() triggers installation if necessary.
+
+
+        // Make sure ARCore is installed and up to date.
+        try {
+            Session mSession = new Session(this);
+            if (mSession == null) {
+                switch (ArCoreApk.getInstance().requestInstall(this, mUserRequestedInstall)) {
+                    case INSTALLED:
+                        // Success, create the AR session.
+                        mSession = new Session(this);
+                        break;
+                    case INSTALL_REQUESTED:
+                        // Ensures next invocation of requestInstall() will either return
+                        // INSTALLED or throw an exception.
+                        mUserRequestedInstall = false;
+                        return;
+                }
+            }
+        } catch (UnavailableUserDeclinedInstallationException e) {
+            // Display an appropriate message to the user and return gracefully.
+            Toast.makeText(this, "TODO: handle exception " + e, Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+        catch(UnavailableDeviceNotCompatibleException e){
+            Toast.makeText(this, "TODO: message for incompat device" + e, Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+        catch(Exception e){
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                    .show();
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this);
+            }
+            finish();
+        }
+    }
+
 }
